2c2085801580d92696c7501c76490c2839c248b6
Lucca Mendes Alves Pellegrini <luccapellegrini@protonmail.com>
2020-08-23 04:45:28 +0000

    st: use Terminus font

--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "xos4 Terminus:style=Regular:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
