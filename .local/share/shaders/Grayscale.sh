25dc220c833bee6a00747b065c36f48068944dfd
Lucca Pellegrini <luccapellegrini@e.email>
2021-09-14 22:30:53 +0000

    add ‘dmenushader’ script and ‘Grayscale’ shader
    ‘dmenushader’ allows the user to select GLSL shaders for picom from
    dmenu. Shaders are stored in ‘$XDG_DATA_HOME/shaders’ as *.glsl files
    and may be paired with a *.sh script to be executed on shader selection.
    
    Includes a ‘Grayscale.glsl’ shader which applies a grayscale filter to
    all windows, as well as a ‘Grayscale.sh’ script which applies a
    grayscale filter to the background image using ImageMagick and resets it
    on termination.

---
+++ .local/share/shaders/Grayscale.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+cp "$(xdg-user-dir PICTURES)"/wallpaper /tmp/wallpaper.bak
+convert /tmp/wallpaper.bak -colorspace gray /tmp/wallpaper
+setbg /tmp/wallpaper && rm /tmp/wallpaper
+
+reset()
+{
+	setbg /tmp/wallpaper.bak && rm /tmp/wallpaper.bak
+	exit 0
+}
+
+trap reset HUP INT TERM
+until ! kill -0 "$1"; do
+	sleep 1
+done
+reset
