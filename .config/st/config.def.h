27ce94499f2ab8189b0bcc561298573812e7a3be
Lucca Pellegrini <luccapellegrini@e.email>
2022-01-21 20:07:13 +0000

    [st] change default font

--- .config/st/config.def.h
+++ .config/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus";
+static char *font = "Source Code Pro";
 static int borderpx = 10;
 
 /*
