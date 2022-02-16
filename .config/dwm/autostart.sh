a6f5afdba132a43bcc6ba9228c9ffbfd221ca429
Lucca Pellegrini <luccapellegrini@e.email>
2022-02-16 22:41:29 +0000

    [dwm] start tabbed st on login

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,6 +1,9 @@
 # layouts
+dwmc viewex 0 && dwmc setlayoutex 1
 dwmc viewex 2 && dwmc setlayoutex 1
-dwmc viewex 3 && dwmc setlayoutex 1
+# dwmc viewex 3 && dwmc setlayoutex 1
 dwmc viewex 0
+
 # programs
+(cd && tabbed st -w) &
 #qutebrowser &
