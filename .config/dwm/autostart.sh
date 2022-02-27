65f2b5bdaf1ace2299c927a220eb500363df5cc2
Lucca Pellegrini <luccapellegrini@e.email>
2022-02-27 17:10:47 +0000

    [dwm] start browser and password manager on login

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,9 +1,10 @@
 # layouts
-dwmc viewex 0 && dwmc setlayoutex 1
 dwmc viewex 2 && dwmc setlayoutex 1
+dwmc viewex 8 && dwmc setlayoutex 1
 # dwmc viewex 3 && dwmc setlayoutex 1
-dwmc viewex 0
+dwmc viewex 0 && dwmc setlayoutex 1
 
 # programs
 (cd && tabbed st -w) &
-#qutebrowser &
+keepassxc &
+qutebrowser &
