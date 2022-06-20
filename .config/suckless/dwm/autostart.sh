6854746bc4a16cec260a589456409d63d8f7ccf7
Lucca Pellegrini <luccapellegrini@e.email>
2022-06-20 13:26:05 +0000

    [dwm] do not automatically open browser

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -1,10 +1,8 @@
 # layouts
 dwmc viewex 2 && dwmc setlayoutex 1
 dwmc viewex 8 && dwmc setlayoutex 1
-# dwmc viewex 3 && dwmc setlayoutex 1
 dwmc viewex 0 && dwmc setlayoutex 1
 
 # programs
 (cd && tabbed st -w) &
 keepassxc &
-qutebrowser &
