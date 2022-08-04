8d74cf810cee4b73916fea0729fe24158e000497
Lucca Pellegrini <luccapellegrini@e.email>
2022-08-04 14:42:47 +0000

    add neomutt config

---
+++ .config/mutt/etc/openfile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Helps open a file with xdg-open from mutt in a external program without weird side effects.
+
+base=$(basename "$1")
+ext="${base##*.}"
+
+file=$(mktemp -u --suffix=".$ext")
+
+cat "$1" > "$file"
+
+setsid xdg-open "$file" >/dev/null 2>&1 &
