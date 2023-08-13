2a60a6a5e53f4cef665915b734108a576b79b5da
Lucca Pellegrini <luccapellegrini@e.email>
2023-08-13 20:28:20 +0000

    dwm: change startup terminal configuration
    Since this is the main terminal, use alcritty. Also call a login shell
    with tmux for every new tab.

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -3,5 +3,5 @@ dwmc viewex 8 && dwmc setlayoutex 1 #&& dwmc togglebar
 dwmc viewex 0 && dwmc setlayoutex 1
 
 # programs
-(cd && tabbed st -w) &
+(cd && tabbed -r2 alacritty --embed xid -e zsh -lc tmux) &
 keepassxc &
