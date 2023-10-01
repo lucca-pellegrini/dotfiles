8618029615c2f5b98dc29124b878b4b7fb2d04c9
Lucca Pellegrini <luccapellegrini@e.email>
2023-10-01 03:13:03 +0000

    dwm: launch in $HOME and don't change the layout
    Changing directory to $HOME on launch is probably something that you
    want in a window manager anyway, though you normally don't have to worry
    about it. Also, since fbc0243, it is no longer necessary to change any
    tag's layout to monocle, because that was made the default.

--- .config/suckless/dwm/autostart.sh
+++ .config/suckless/dwm/autostart.sh
@@ -1,6 +1,5 @@
-# layouts
-dwmc viewex 8 && dwmc setlayoutex 1 #&& dwmc togglebar
-dwmc viewex 0 && dwmc setlayoutex 1
+# start on $HOME
+cd
 
 # programs
 (cd && tabbed -r2 alacritty --embed xid -e zsh -lc tmux) &
