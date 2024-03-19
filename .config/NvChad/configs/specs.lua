a2a2c34f57367ccd1cb3cbc9ccbe702739c20356
Lucca Pellegrini <luccapellegrini@e.email>
2024-03-19 08:13:24 -0300

    nvim: add configuration for the ‘specs’ plugin
    As it is, the plugin is not installed or loaded, but whenever I do
    install it, I'd like its configuration file to be already present.

---
+++ .config/NvChad/configs/specs.lua
@@ -0,0 +1,19 @@
+local specs = require "specs"
+
+specs.setup({
+	show_jumps = true,
+	min_jump = 10,
+	popup = {
+		delay_ms = 0, -- delay before popup displays
+		inc_ms = 15, -- time increments used for fade/resize effects
+		blend = 50, -- starting blend, between 0-100 (fully transparent), see :h winblend
+		width = 20,
+		winhl = "PMenu",
+		fader = specs.pulse_fader,
+		resizer = specs.slide_resizer,
+	},
+	ignore_filetypes = {},
+	ignore_buftypes = {
+		nofile = true,
+	},
+})
