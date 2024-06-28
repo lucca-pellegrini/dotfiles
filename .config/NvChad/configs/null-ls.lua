56b15dd7b56cdc0ccde690dfc7d0aebe7f9127ea
Lucca Pellegrini <luccapellegrini@e.email>
2024-06-28 17:04:14 -0300

    nvim: add Python formatting with `blue`
    Again, we haven't installed it explicitly in the Mason configuration, so
    you have to install it manually, either with Mason or to the system
    itself. See 34ba4b5.
    
    Also, format the `null-ls.lua` configuration file.

--- .config/NvChad/configs/null-ls.lua
+++ .config/NvChad/configs/null-ls.lua
@@ -4,38 +4,41 @@ local b = null_ls.builtins
 
 local sources = {
 
-	-- webdev stuff
-	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+  -- webdev stuff
+  b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
-	-- Lua
-	b.formatting.stylua,
+  -- Lua
+  b.formatting.stylua,
 
-	-- C
-	b.formatting.clang_format.with({
-		extra_args = { -- Use my custom config by default
-			"--style=file:" .. os.getenv("XDG_CONFIG_HOME") .. "/NvChad/configs/clang-format",
-		},
-	}),
+  -- C
+  b.formatting.clang_format.with({
+    -- extra_args = { -- Use my custom config by default
+    -- 	"--style=file:" .. os.getenv("XDG_CONFIG_HOME") .. "/NvChad/configs/clang-format",
+    -- },
+  }),
 
-	-- Go
-	b.formatting.gofumpt,
-	b.formatting.goimports_reviser.with({
-		extra_args = {
-			"-rm-unused",
-			"-set-alias",
-			"-use-cache",
-		},
-	}),
-	b.formatting.golines.with({
-		extra_args = {
-			"--max-len=" .. 80,
-			"--tab-len=" .. 2, -- This is our current setting in init.lua
-		},
-	}),
+  -- Go
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser.with({
+    extra_args = {
+      "-rm-unused",
+      "-set-alias",
+      "-use-cache",
+    },
+  }),
+  b.formatting.golines.with({
+    extra_args = {
+      "--max-len=" .. 80,
+      "--tab-len=" .. 2, -- This is our current setting in init.lua
+    },
+  }),
+
+  -- Python
+  b.formatting.blue,
 }
 
 null_ls.setup({
-	debug = true,
-	sources = sources,
+  debug = true,
+  sources = sources,
 })
