d1cc9bf2bb853d038098d1d773bef1d6d3232b57
Lucca Pellegrini <luccapellegrini@e.email>
2024-08-09 16:22:01 -0300

    nvim: add Java language server

--- .config/NvChad/configs/lspconfig.lua
+++ .config/NvChad/configs/lspconfig.lua
@@ -6,7 +6,16 @@ local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "texlab", "ltex", "rust_analyzer", "pyright" }
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "texlab",
+  "ltex",
+  "rust_analyzer",
+  "pyright",
+  "jdtls",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup({
