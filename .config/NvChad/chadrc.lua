16096234fdbc81a7c7b865fb07d4fb4bdd879215
Lucca Pellegrini <luccapellegrini@e.email>
2024-11-19 15:14:52 -0300

    nvim: make `vscode_dark` the default dark theme

--- .config/NvChad/chadrc.lua
+++ .config/NvChad/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "oxocarbon",
-  theme_toggle = { "one_light", "oxocarbon" },
+  theme = "vscode_dark",
+  theme_toggle = { "one_light", "vscode_dark" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
