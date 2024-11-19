59dc0e0a1a23ac94a80dcb7b911d57199aa6f6e2
Lucca Pellegrini <luccapellegrini@e.email>
2024-11-19 15:16:40 -0300

    nvim: add nvim-surround plugin

--- .config/NvChad/plugins.lua
+++ .config/NvChad/plugins.lua
@@ -22,6 +22,18 @@ local plugins = {
     },
   },
 
+  -- Manage delimiter pairs easily
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+
   -- Git wrapper
   {
     "tpope/vim-fugitive",
