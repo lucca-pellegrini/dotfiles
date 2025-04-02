cef1d7a2f309882547b05c18c652c256b964e87d
Lucca Pellegrini <lucca@verticordia.com>
2025-04-02 18:37:40 -0300

    dwm: add keybind for pulsemixer

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -131,6 +131,7 @@ static Key keys[] = {
 	{ MODKEY|ALTKEY,                XK_m,      spawn,          SHCMD("st -n float -g 120x36 -e ncmpcpp") },
 	{ MODKEY|ALTKEY,                XK_v,      spawn,          SHCMD("st -e vim") },
 	{ MODKEY|ALTKEY,                XK_a,      spawn,          SHCMD("st -e alsamixer") },
+	{ MODKEY|ALTKEY,                XK_p,      spawn,          SHCMD("st -e pulsemixer") },
 	{ MODKEY|ALTKEY,                XK_w,      spawn,          SHCMD("dmenuqbpm") },
 
 	/* scripts */
