db007804e7938f9c981d8ebbe57bec9035715e57
Lucca Pellegrini <lucca@verticordia.com>
2025-09-02 14:54:15 -0300

    nvim: switch AI plugin to something that runs in agent mode

--- .config/NvChad/plugins.lua
+++ .config/NvChad/plugins.lua
@@ -115,53 +115,27 @@ local plugins = {
 
   -- Generative AI integration
   {
-    "yetone/avante.nvim",
-    tag = 'v0.0.23',
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "openai",
-      openai = {
-        api_key_name = { "pass", "net/api/openai" },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    'NickvanDyke/opencode.nvim',
     dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      -- Recommended for a better prompt input, and required to use opencode.nvim's embedded terminal. Otherwise optional.
+      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
+    },
+    ---@type opencode.Opts
+    opts = {
+      -- Your configuration, if any
+    },
+    keys = {
+      -- Recommended keymaps
+      { '<leader>oa', function() require('opencode').ask('@cursor: ') end,                          desc = 'Ask opencode',                 mode = 'n', },
+      { '<leader>oa', function() require('opencode').ask('@selection: ') end,                       desc = 'Ask opencode about selection', mode = 'v', },
+      { '<leader>ot', function() require('opencode').toggle() end,                                  desc = 'Toggle embedded opencode', },
+      { '<leader>on', function() require('opencode').command('session_new') end,                    desc = 'New session', },
+      { '<leader>oy', function() require('opencode').command('messages_copy') end,                  desc = 'Copy last message', },
+      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end,          desc = 'Scroll messages up', },
+      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end,        desc = 'Scroll messages down', },
+      { '<leader>op', function() require('opencode').select_prompt() end,                           desc = 'Select prompt',                mode = { 'n', 'v', }, },
+      -- Example: keymap for custom prompt
+      { '<leader>oe', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
     },
   },
 
